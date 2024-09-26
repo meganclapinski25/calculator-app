5c4da555f0425b100437875fabe50b3ea11997fd
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2024-09-26 18:10:29 +0000

    template and code

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        operation = request.form['operation']
+
+        if operation == 'add':
+            result = num1 + num2
+        elif operation == 'subtract':
+            result = num1 - num2
+        elif operation == 'multiply':
+            result = num1 * num2
+        elif operation == 'divide':
+            if num2 == 0:
+                result = "Division by zero not allowed!"
+            else:
+                result = num1 / num2
+        else:
+            result = "Invalid operation"
+
+        return render_template('index.html', result=result)
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
